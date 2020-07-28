37f7e82a25d9966cd285998dcb00a760ae06f710
Angela Stephens <angelastephens317@gmail.com>
2020-07-28 23:49:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,20 +6,20 @@ class Student
   attr_reader :id
   attr_accessor :name, :grade
   
-def initialize(name,grade)
-  @name=name
-  @grade=grade
-end
+  def initialize(name,grade)
+    @name=name
+    @grade=grade
+  end
 
-def self.create_table
-  sql = <<- SQL
-  CREATE TABLE IF NOT EXSITS students(
-    id INTEGER PRIMARY KEY
-    name TEXT
-    grade TEXT
-  );
-  SQL
-  DB[:conn].execute(sql)
-end
+  def self.create_table
+    sql = <<- SQL
+    CREATE TABLE IF NOT EXSITS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
   
 end
