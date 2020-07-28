6b2b9b5b1103688ebc2df6a46ed40218f0520442
Angela Stephens <angelastephens317@gmail.com>
2020-07-28 23:46:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,18 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  
+  attr_reader :id
+  attr_accessor :name, :grade
+  
+def initialize(name,grade)
+  @name=name
+  @grade=grade
+end
+
+def self.create_table
+  sql = <<- SQL
+  CREATE TABLE IF NOT 
   
 end
